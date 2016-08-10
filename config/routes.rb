4a445ba57831b09c2dd874dae3de86f7b86b3a8e
Kairav Parekh <kparekh01@gmail.com>
2016-08-10 12:36:38 -0400

    Updated app for devise config and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do #if user is signed in , show them the index page
+   authenticated :user do
+     root 'links#index', as: :authenticated_root
+   end
+
+   unauthenticated do #if no user is signed in, show them the sign in page.
+     root 'devise/registrations#new', as: :unauthenticated_root
+   end
+ end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
